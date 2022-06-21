d744e86f5177d8b0b47109075de07bd6d360ee42
Ruslan <rrydvans@gmail.com>
2022-06-21 09:38:30 -0700

    fixed robots

--- scrapy_interview/settings.py
+++ scrapy_interview/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapy_interview.spiders'
 #USER_AGENT = 'scrapy_interview (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+#ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
